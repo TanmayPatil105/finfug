964f8fa0ae187cb36366cc1a46a433a5d85cd199
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-08-18 22:44:01 +0530

    Generate username if null

--- src/app/MainApp.js
+++ src/app/MainApp.js
@@ -1,37 +1,50 @@
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react'
 import { Container , Typography, TextField, Grid, Button} from '@mui/material';
 import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator';
 
-export default class MainApp extends Component {
+export default function MainApp() {
 
-    const [displayName, setDisplayName] = useState('')
+    const [displayName, setDisplayName] = useState('');
 
-    JoinRoom(){
-
+    function GenerateName(){
+        const shortName = uniqueNamesGenerator({
+            dictionaries: [adjectives, animals, colors], // colors can be omitted here as not used
+            length: 2,
+            separator: '-'
+          });
+       
+        setDisplayName(shortName)
     }
-    
-    CreateAndJoinRoom(){
-        
+
+    function HandleJoinRoom(){
+        if (displayName === ""){
+            GenerateName()
+        }
+        console.log(displayName)
     }
 
-    render() {
-        return (
+    function HandleCreateAndJoinRoom(){
+        if (displayName === ""){
+            GenerateName()
+        }
+    }
+
+    return (
         <div>
-            <Container maxWidth="sm">
-                <div style={{ textAlign: 'center', margin: '50px auto' }}>
-                    <Typography variant="h4">FinFugal</Typography>
-                    <TextField label="Display Name" fullWidth variant="outlined" margin="normal" />
-                    <Grid container spacing={2} justifyContent="center">
-                        <Grid item>
-                            <Button variant="contained" color="primary" onClick={this.JoinRoom}>Join Room</Button>
-                        </Grid>
-                        <Grid item>
-                            <Button variant="contained" color="primary" onClick={this.CreateAndJoinRoom}>Create Room</Button>
-                        </Grid>
+        <Container maxWidth="sm">
+            <div style={{ textAlign: 'center', margin: '50px auto' }}>
+                <Typography variant="h4">FinFugal</Typography>
+                <TextField label="Display Name" fullWidth variant="outlined" margin="normal" onChange={(event) => { setDisplayName(event.target.value);}}/>
+                <Grid container spacing={2} justifyContent="center">
+                    <Grid item>
+                        <Button variant="contained" color="primary" onClick={HandleJoinRoom}>Join Room</Button>
                     </Grid>
-                </div>
-            </Container>
+                    <Grid item>
+                        <Button variant="contained" color="primary" onClick={HandleCreateAndJoinRoom}>Create Room</Button>
+                    </Grid>
+                </Grid>
+            </div>
+        </Container>
         </div>
-        )
-    }
+    )
 }
