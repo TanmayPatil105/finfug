6638b4461280bd2c9b4a2d865d38b7ec4cb9468e
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-08-19 19:58:25 +0530

    Display Room messages

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Container, Typography, List, ListItem, ListItemText} from '@mui/material';
 
-function Room() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function Room(props) {
+
+    const room = props.room;
+
+    return (
+        <Container maxWidth="sm">
+            <Typography variant="h5">Messages:</Typography>
+            <div
+                style={{
+                    maxHeight: '600px',
+                    maxWidth: '800px',
+                    overflowY: 'auto',
+                }}
+                id="messages-box" 
+            >
+              <List>
+                {room.messages.map((message, index) => (
+                    <ListItem key={index}>
+                        <ListItemText primary={message.text} secondary={`Sent by: ${message.userId}`} />
+                    </ListItem>
+                ))}
+              </List>
+            </div>
+            
+        </Container>
+    );
 }
-
-export default Room;
