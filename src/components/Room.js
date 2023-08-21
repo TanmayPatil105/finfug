fdf146bf6c57df2a81b075eb571bdc9231eb4d79
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-08-22 00:27:17 +0530

    Improve chat window

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,30 +1,42 @@
 import React from 'react';
-import { Container, Typography, List, ListItem, ListItemText} from '@mui/material';
+import { Container, List, ListItem, ListItemText, TextField} from '@mui/material';
+import classnames from 'classnames';
+import './Room.css';
 
 export default function Room(props) {
 
     const room = props.room;
 
+    let userId = 'cjfhin0vbiv3iu26hqjg';
+
     return (
-        <Container maxWidth="sm">
-            <Typography variant="h5">Messages:</Typography>
+        <Container maxWidth="sm" className="chat-window">
             <div
                 style={{
                     maxHeight: '600px',
-                    maxWidth: '800px',
+                    maxWidth: '1600px',
                     overflowY: 'auto',
                 }}
                 id="messages-box" 
             >
-              <List>
+              <List className='chat-messages'>
                 {room.messages.map((message, index) => (
                     <ListItem key={index}>
-                        <ListItemText primary={message.text} secondary={`Sent by: ${message.userId}`} />
+                        <ListItemText
+                            className={classnames({
+                                'user-message': message.userId === userId,
+                                'other-message': message.userId !== userId,
+                            })}
+                            primary={message.text}
+                            secondary={message.userId !== userId ? room.participants.find(
+                                partcipant => partcipant.userId === message.userId
+                            ).name : 'You'}
+                        />
                     </ListItem>
                 ))}
               </List>
+              <TextField fullWidth variant="outlined" margin="normal" placeholder='Message' className='enter-message'/>
             </div>
-            
         </Container>
     );
 }
