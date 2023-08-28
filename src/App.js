41a9d88ee77750e09684929f88631c15784aa783
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-08-28 19:24:20 +0530

    Rename things

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import { useState } from 'react'
 import './App.css';
 import MainApp from './app/MainApp';
 import EnterRoom from './components/EnterRoom';
 
 function App() {
-   
-  const [notJoin, setNotJoin] = useState(true);
-
   return (
     <div style={{margin: 0, padding: 0, backgroundSize: 'cover', minHeight: '100vh' }}>
       <MainApp />
