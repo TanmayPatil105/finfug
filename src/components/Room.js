10cb54ce8d5f02cf0b9419ce0d57e333b46aa32f
Tanmay Patil <tanmaynpatil105@gmail.com>
2023-09-01 18:49:40 +0530

    Improve room UI

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, List, ListItem, ListItemText, TextField, Grid} from '@mui/material';
 import Fab from '@material-ui/core/Fab';
 import SendIcon from '@material-ui/icons/Send';
 import classnames from 'classnames';
 import './Room.css';
+import { sendMessage } from '../api/Api';
 
 export default function Room(props) {
 
-    const room = props.room;
+    const [text, setText] = useState(''); 
 
-    let userId = props.userId;
+    let room = JSON.parse(props.room);
+    console.log(room);
+    let UserId = props.userId;
+    console.log(UserId);
+    UserId='cjotd10vbiv5ofe8vqhg';
+
+    async function HandleSendMessage(roomid){
+        await sendMessage(UserId, room.SessionId, text);
+        // setRoom(data);
+        // setInRoom(true);
+    }
 
     return (
         <Container maxWidth="sm" className="chat-window">
@@ -22,27 +33,27 @@ export default function Room(props) {
                 id="messages-box" 
             >
               <List className='chat-messages'>
-                {room.messages.map((message, index) => (
+                {room.Messages && room.Messages.map((Message, index) => (
                     <ListItem key={index}>
                         <ListItemText
                             className={classnames({
-                                'user-message': message.userId === userId,
-                                'other-message': message.userId !== userId,
+                                'user-message': Message.UserId === UserId,
+                                'other-message': Message.UserId !== UserId,
                             })}
-                            primary={message.text}
-                            secondary={message.userId !== userId ? room.participants.find(
-                                partcipant => partcipant.userId === message.userId
-                            ).name : 'You'}
+                            primary={Message.Text}
+                            secondary={Message.UserId !== UserId ? room.Participants.find(
+                                Participant => Participant.UserId === Message.UserId
+                            ).Name : 'You'}
                         />
                     </ListItem>
                 ))}
               </List>
               <Grid container style={{padding: '20px'}}>
                     <Grid item xs={11} align="left">
-                    <TextField placeholder="Message" fullWidth />
+                    <TextField placeholder="Message" fullWidth onChange={(event) => { setText(event.target.value);}}/>
                     </Grid>
                     <Grid xs={1} align="center" className="input">
-                        <Fab size="small" color="secondary" aria-label="add"><SendIcon /></Fab>
+                        <Fab size="small" color="secondary" aria-label="add" onClick={HandleSendMessage}><SendIcon /></Fab>
                     </Grid>
                 </Grid>
             </div>
